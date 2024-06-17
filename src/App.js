8b1702159297e5a7d67c5a7990959fd66316d4bc
Ni <daisytsai11336@gmail.com>
2024-06-17 10:17:57 +0800

    艾邁格test02，功能撰寫至按鈕點選邏輯與按鈕樣式改變。

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.scss";
+import {useState, useEffect, useRef} from "react";
 
-function App() {
+export default function App() {
+  const [btnSelected, setBtnSelected] = useState(false);
+  const [mountain, setMountain] = useState("");
+  const [sunMoon, setSunMoon] = useState("");
+
+  const handleBtnSelected = () => {
+    setBtnSelected(!btnSelected);
+  }
+  const handleMountain = (mountainName) => {
+    if (mountain === mountainName) {
+      setMountain(""); // 取消選中
+    } else {
+      setMountain(mountainName); // 設置為選中
+    }
+  };
+
+  const handleSunMoon = (sunMoonName) => {
+    if (sunMoon === sunMoonName) {
+      setSunMoon(""); // 取消選中
+    } else {
+      setSunMoon(sunMoonName); // 設置為選中
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="top-box">
+        <div>&nbsp;</div>
+        <div>&</div>
+        <div>&nbsp;</div>
+      </div>
+      <canvas
+        id="canvas"
+        width={894}
+        height={952}
+        style={{ backgroundColor: "white" }}
+      ></canvas>
+      <div className="btns">
+        <div className={`btn ${mountain === "mountain1" ? "selected-btn" : ""}`} onClick={() => {
+          handleMountain("mountain1");
+          handleBtnSelected(true);
+        }}>
+          Mountain 1
+        </div>
+        <div className={`btn ${mountain === "mountain2" ? "selected-btn" : ""}`} onClick={() => {
+          handleMountain("mountain2");
+          handleBtnSelected(true);
+        }}>
+          Mountain 2
+        </div>
+        <div className={`btn ${sunMoon === "sun" ? "selected-btn" : ""}`} onClick={() => {
+          handleSunMoon("sun");
+          handleBtnSelected(true);
+        }}>
+          Sun
+        </div>
+        <div className={`btn ${sunMoon === "moon" ? "selected-btn" : ""}`} onClick={() => {
+          handleSunMoon("moon");
+          handleBtnSelected(true);
+        }}>
+          Moon
+        </div>
+        <div className="btn btn-download" onClick={() => {}}>
+          Download card
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
